d25fecfaaf068f5980c8ce71ae820bab913076c5
Manjunath Shivanna <ms5655@rit.edu>
2016-12-01 01:40:23 -0500

    camera and render layer merging.

--- app/src/main/java/com/myapp/manju/arstrike/EngineActivity.java
+++ app/src/main/java/com/myapp/manju/arstrike/EngineActivity.java
@@ -8,12 +8,21 @@ import android.view.Menu;
 
 import com.threed.jpct.FrameBuffer;
 import com.threed.jpct.Light;
+import com.threed.jpct.Loader;
+import com.threed.jpct.Matrix;
 import com.threed.jpct.Object3D;
 import com.threed.jpct.Primitives;
 import com.threed.jpct.RGBColor;
+import com.threed.jpct.SimpleVector;
 import com.threed.jpct.Texture;
 import com.threed.jpct.TextureManager;
 import com.threed.jpct.World;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -71,6 +80,33 @@ public class EngineActivity extends Activity implements GLSurfaceView.Renderer {
         sphere.translate(0,0.45f,0);
         sphere.setTexture("t2");
 
+        /*
+        Object3D thing;
+        thing = loadModel("resources/3dObj" + "IronMan" + ".obj","resources/3dObj" + "IronMan" + ".mtl", 1);
+        thing.translate(0,0,0);
+        thing.setTexture("t1");
+        thing.build();
+        world.addObject(thing); */
+
+        Object3D thing=null;
+        try {
+            thing = loadModel("assets/" + "altair" + ".3ds", 1);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        thing.translate(0,0,0);
+        thing.build();
+        thing.setTexture("t1");
+        world.addObject(thing);
+
+        /*
+        try {
+            cube=Object3D.mergeAll(Loader.load3DS(getResources().getAssets().open("altair.3ds", 1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
         cube = Primitives.getCube(1);
         cube.translate(5,0,10);
         cube.setTexture("t1");
@@ -99,4 +135,50 @@ public class EngineActivity extends Activity implements GLSurfaceView.Renderer {
 
         fb.display();
     }
+
+
+    private Object3D loadModel(String objName,String mtlName, float scale)  {
+
+        InputStream obj = new ByteArrayInputStream( objName.getBytes() );
+        InputStream mtl = new ByteArrayInputStream( mtlName.getBytes() );
+
+        Object3D[] model = Loader.loadOBJ(obj,mtl,scale);
+        Object3D o3d = new Object3D(0);
+        Object3D temp = null;
+        for (int i = 0; i < model.length; i++) {
+            temp = model[i];
+            temp.setCenter(SimpleVector.ORIGIN);
+            temp.rotateX((float)( -.5*Math.PI));
+            temp.rotateMesh();
+            temp.setRotationMatrix(new Matrix());
+            o3d = Object3D.mergeObjects(o3d, temp);
+            o3d.build();
+        }
+        return o3d;
+    }
+
+    private Object3D loadModel(String filename, float scale) throws UnsupportedEncodingException {
+        InputStream stream=null;
+        try {
+             stream = this.getAssets().open("test.3ds");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //InputStream stream = new ByteArrayInputStream(filename.getBytes("UTF-8"));
+        Object3D[] model = Loader.load3DS(stream, scale);
+        Object3D o3d = new Object3D(0);
+        Object3D temp = null;
+        for (int i = 0; i < model.length; i++) {
+            temp = model[i];
+            temp.setCenter(SimpleVector.ORIGIN);
+            temp.rotateX((float)( -.5*Math.PI));
+            temp.rotateMesh();
+            temp.setRotationMatrix(new Matrix());
+            o3d = Object3D.mergeObjects(o3d, temp);
+            o3d.build();
+        }
+        return o3d;
+    }
+
 }
