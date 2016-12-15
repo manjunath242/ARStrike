202c884024071e95d6f801fe4abf37623b6de4b9
Manjunath Shivanna <manjunaths242@gmail.com>
2016-12-15 14:19:20 -0500

    Finalversion

---
+++ app/src/main/java/com/myapp/manju/arstrike/Variables.java
@@ -0,0 +1,12 @@
+package com.myapp.manju.arstrike;
+
+import com.threed.jpct.SimpleVector;
+
+/**
+ * Created by manju on 12/15/2016.
+ */
+
+public class Variables {
+
+    //public static SimpleVector position;
+}
